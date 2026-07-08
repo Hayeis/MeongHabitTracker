57773250f69ae9a28c62ac48b4ef3834a36beec5
Ponpon <alfonsusbl@gmail.com>
2026-07-08 12:49:08 +0700

    Nambah Data

--- app/src/main/java/com/alfons/meonghabittracker/model/GlobalData.kt
+++ app/src/main/java/com/alfons/meonghabittracker/model/GlobalData.kt
@@ -4,6 +4,7 @@ object GlobalData {
     val habits = arrayListOf(
         Habit("1", "Minum Air", "Minum 8 gelas sehari", 8, 0,"Gelas", "baseline_water_drop_24"),
         Habit("2", "Olahraga", "Push up & Dumbbell", 30, 0,"Menit", "baseline_directions_run_24"),
-        Habit("3", "Membaca", "Baca buku sebelum tidur", 3, 0,"Bab", "baseline_menu_book_24")
+        Habit("3", "Membaca", "Baca buku sebelum tidur", 3, 0,"Bab", "baseline_menu_book_24"),
+        Habit("4", "Ngoding", "Ngoding Proyek WFP", 1, 0,"Proyek", "baseline_code_24")
     )
 }
