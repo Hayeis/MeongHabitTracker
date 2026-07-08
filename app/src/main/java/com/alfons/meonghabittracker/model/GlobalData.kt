5ec2b708fe807a1b1e944d68015d52630cf37714
Ponpon <alfonsusbl@gmail.com>
2026-07-08 12:40:48 +0700

    Nambah Vector Asset, dan perbesar UI Create Habit

--- app/src/main/java/com/alfons/meonghabittracker/model/GlobalData.kt
+++ app/src/main/java/com/alfons/meonghabittracker/model/GlobalData.kt
@@ -2,8 +2,8 @@ package com.alfons.meonghabittracker.model
 
 object GlobalData {
     val habits = arrayListOf(
-        Habit("1", "Minum Air", "Minum 8 gelas sehari", 8, 0,"Gelas", "ic_water"),
-        Habit("2", "Olahraga", "Push up & Dumbbell", 30, 0,"Menit", "ic_fitness"),
-        Habit("3", "Membaca", "Baca buku sebelum tidur", 3, 0,"Bab", "ic_book")
+        Habit("1", "Minum Air", "Minum 8 gelas sehari", 8, 0,"Gelas", "baseline_water_drop_24"),
+        Habit("2", "Olahraga", "Push up & Dumbbell", 30, 0,"Menit", "baseline_directions_run_24"),
+        Habit("3", "Membaca", "Baca buku sebelum tidur", 3, 0,"Bab", "baseline_menu_book_24")
     )
 }
