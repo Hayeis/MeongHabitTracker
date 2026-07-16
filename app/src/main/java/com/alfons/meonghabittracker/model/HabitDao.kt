7836d5d23ab78d810814b2c4041eb53509ced05b
Ngawikwoks <alfonsusbls@gmail.com>
2026-07-17 01:18:18 +0700

    Setting Awal Database

---
+++ app/src/main/java/com/alfons/meonghabittracker/model/HabitDao.kt
@@ -0,0 +1,21 @@
+package com.alfons.meonghabittracker.model
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface HabitDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg habit: Habit)
+
+    @Query("SELECT * FROM habit")
+    fun selectAllHabit(): List<Habit>
+
+    @Query("SELECT * FROM habit WHERE id = :userId")
+    fun selectHabit(userId: Int): Habit
+
+    @Delete
+    fun deleteHabit(habit:Habit)
+}
--- app/src/main/java/com/alfons/meonghabittracker/model/Model.kt
+++ app/src/main/java/com/alfons/meonghabittracker/model/Model.kt
@@ -1,11 +1,40 @@
 package com.alfons.meonghabittracker.model
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
 
+@Entity
+data class User(
+    @PrimaryKey(autoGenerate = true)
+    var userId: Int = 0,
+    var username: String = "",
+    var password: String = ""
+)
+
+@Entity(
+foreignKeys = [
+    ForeignKey(
+        entity = User::class,
+        parentColumns = ["userId"],
+        childColumns = ["ownerId"],
+        onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class Habit(
-    var id: String?,
-    var name: String?,
-    var description: String?,
-    var goal: Int?,
-    var currentProgress: Int = 0,
-    var unit: String?,
-    var icon: String?
+    @PrimaryKey(autoGenerate = true)
+    var id:Int = 0,
+    @ColumnInfo(name="name")
+    var name:String,
+    @ColumnInfo(name="description")
+    var description:String,
+    @ColumnInfo(name="goal")
+    var goal:Int,
+    @ColumnInfo(name="currentProgress")
+    var currentProgress: Int,
+    @ColumnInfo(name="unit")
+    var unit:String,
+    @ColumnInfo(name="icon")
+    var icon:String,
 )
